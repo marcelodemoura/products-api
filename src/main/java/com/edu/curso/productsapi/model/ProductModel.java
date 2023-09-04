101ba6c203a1c60807f68fae68245c269af01295
Marcelo de Moura Cavalcanti Junior <marcelo.cavalcanti@mv.com.br>
2023-09-04 10:26:36 -0300

    atualização

--- src/main/java/com/edu/curso/productsapi/model/ProductModel.java
+++ src/main/java/com/edu/curso/productsapi/model/ProductModel.java
@@ -8,16 +8,16 @@ import org.springframework.hateoas.RepresentationModel;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.UUID;
+
 @Getter
 @Setter
 @Entity
 @Table(name = "tb_product")
-public class ProductModel extends RepresentationModel< ProductModel> implements Serializable {
+public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
     private static final long serialVersionUID = 1;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-
     private UUID idProduct;
     private String name;
     private BigDecimal value;
